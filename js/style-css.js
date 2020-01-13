ebbc150db2c8cfbc44cbf94d1bfd2bb1fa5d66b1
prakash-dot-08 <prakash@dotworld.in>
2020-01-13 15:41:49 +0530

    Updates

---
+++ js/style-css.js
@@ -0,0 +1,7 @@
+
+const bun=require('../css/normalize.css')
+const css= require('../css/detheme.css');
+const theme= require('../css/kergan.detheme.css');
+
+
+console.log('welcome to interlock group');
